f0c612875d9ea2d745993f7794e1c0daefce28e2
Randula <randula@linearsix.com>
2024-04-20 19:07:52 +0530

    remove the code project

--- drafts/Blockchain Based Lease Agreement Management Tool on Fuel Network/ui/frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { OwnerComponent } from './owner/owner.component'
-import { TenantComponent } from './tenant/tenant.component'
-
-export const routes: Routes = [
-    { path: 'owner', component: OwnerComponent },
-    { path: 'tenant', component: TenantComponent }
-];
